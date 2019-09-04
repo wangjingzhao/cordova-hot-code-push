ee7838acd07e32f4ad672cca11fa98b250473da9
wangjingzhao <517346462@qq.com>
2019-09-04 09:03:02 +0800

    尝试修复xcode不是cordova 插件的bug

--- scripts/lib/iosWKWebViewEngineSupport.js
+++ scripts/lib/iosWKWebViewEngineSupport.js
@@ -6,6 +6,7 @@ Mainly, it has only two method: to activate and to deactivate swift support in t
 var path = require('path');
 var fs = require('fs');
 var strFormat = require('util').format;
+var xcode = reauire('xcode');
 var COMMENT_KEY = /_comment$/;
 var WKWEBVIEW_PLUGIN_NAME = 'cordova-plugin-wkwebview-engine';
 var WKWEBVIEW_MACRO = 'WK_WEBVIEW_ENGINE_IS_USED';
@@ -14,6 +15,7 @@ var context;
 var projectRoot;
 var projectName;
 var iosPlatformPath;
+
 
 module.exports = {
   setWKWebViewEngineMacro: setWKWebViewEngineMacro
@@ -68,7 +70,12 @@ function isDirectoryExists(dir) {
  */
 function loadProjectFile() {
   try {
-    return loadProjectFile_cordova_7_and_above();
+    return loadProjectFile_cordova_9_and_above();
+  } catch(e) {
+  }
+
+  try {
+    return loadProjectFile_cordova_7_and_8();
   } catch(e) {
   }
   
@@ -97,9 +104,24 @@ function loadProjectFile_cordova_5_and_6() {
   return platformIos.parseProjectFile(iosPlatformPath);
 }
 
-function loadProjectFile_cordova_7_and_above() {
+function loadProjectFile_cordova_7_and_8() {
   var pbxPath = path.join(iosPlatformPath, projectName + '.xcodeproj', 'project.pbxproj');
   var xcodeproj = context.requireCordovaModule('xcode').project(pbxPath);
+  xcodeproj.parseSync();
+
+  var saveProj = function() {
+    fs.writeFileSync(pbxPath, xcodeproj.writeSync());
+  };
+
+  return {
+    xcode: xcodeproj,
+    write: saveProj
+  };
+}
+
+function loadProjectFile_cordova_9_and_above() {
+  var pbxPath = path.join(iosPlatformPath, projectName + '.xcodeproj', 'project.pbxproj');
+  var xcodeproj = xcode.project(pbxPath);
   xcodeproj.parseSync();
 
   var saveProj = function() {
